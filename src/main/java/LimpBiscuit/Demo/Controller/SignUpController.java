bd1d51257a444a2d6e21df502a61969969239efb
Aleks <aleks.kozaev@gmail.com>
2018-09-26 13:41:28 +0300

    hibernate dependencies

--- src/main/java/LimpBiscuit/Demo/Controller/SignUpController.java
+++ src/main/java/LimpBiscuit/Demo/Controller/SignUpController.java
@@ -2,7 +2,10 @@ package LimpBiscuit.Demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SignUpController {
@@ -11,5 +14,12 @@ public class SignUpController {
     public String index(Model model) {
         model.addAttribute("text", "Hello");
         return "SignUp";
+    }
+
+    @PostMapping("/signup")
+    public String hello(ModelMap map, @RequestParam String userInput) {
+        /*your code goes here*/
+        map.put("userInput", userInput);
+        return "jsp1";   //first jsp name
     }
 }
