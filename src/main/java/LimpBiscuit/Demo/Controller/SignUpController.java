2f2fcac02a0dcecc1150e10e1cd960400829a0e7
Aleks <aleks.kozaev@gmail.com>
2018-09-28 08:37:18 +0300

    user form

--- src/main/java/LimpBiscuit/Demo/Controller/SignUpController.java
+++ src/main/java/LimpBiscuit/Demo/Controller/SignUpController.java
@@ -1,25 +1,43 @@
 package LimpBiscuit.Demo.Controller;
 
+import LimpBiscuit.Demo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SignUpController {
 
-    @RequestMapping("/signup")
-    public String index(Model model) {
-        model.addAttribute("text", "Hello");
-        return "SignUp";
+//    @RequestMapping(value="/signup", method= RequestMethod.GET)
+//    public ModelAndView getSignupForm() {
+//        ModelAndView model = new ModelAndView("Signup");
+//
+//        return model;
+//    }
+//
+//    @RequestMapping(value="/SignupSubmit", method= RequestMethod.POST)
+//    public ModelAndView submitSignupForm(@RequestParam("email") String email, @RequestParam("psw") String password,
+//            @RequestParam("psw-repeat") String password_repeat) {
+//
+//        ModelAndView model = new ModelAndView("SignupSuccess");
+//        model.addObject("msg", ""+email + " "+password);
+//
+//        return model;
+//    }
+
+    @GetMapping("/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
     }
 
     @PostMapping("/signup")
-    public String hello(ModelMap map, @RequestParam String userInput) {
-        /*your code goes here*/
-        map.put("userInput", userInput);
-        return "jsp1";   //first jsp name
+    public String signupSubmit(@ModelAttribute User user) {
+        if(user.getPassword().equals(user.getPassword_repeat())){
+            return "signupsuccess";
+        }
+        return "signup";
     }
 }
