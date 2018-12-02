d2a40878ef59abfdc2db37f8096d709c582fec5e
Anette Jaakson <anettejaakson@gmail.com>
2018-12-02 13:58:49 +0200

    eemalda cypress

--- test/cypress/integration/test.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Test tests", () => {
-    it("Visits site", () => {
-        cy.visit("routinestarter.herokuapp.com");
-        cy.contains("Try it").click();
-
-        cy.contains("SIIN2 PEAKS");
-    });
-});
